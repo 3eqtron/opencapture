9b03e731b3b2182247a92cbfb2f92848fbdb1b1e
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2018-01-24 21:24:53 +0100

    #4 ci

--- ci/docker_install_mail_server.sh
+++ ci/docker_install_mail_server.sh
@@ -13,8 +13,26 @@ export DEBIAN_FRONTEND=noninteractive \
 && echo 'virtual_minimum_uid = 100' >> /etc/postfix/main.cf \
 && echo 'virtual_uid_maps = static:65534' >> /etc/postfix/main.cf \
 && echo 'virtual_gid_maps = static:8' >> /etc/postfix/main.cf \
-&& less /etc/postfix/main.cf \
-&& less ci/docker_install_mail_server.sh
-
-
-
+&& touch /etc/postfix/virtual_mailbox \
+&& echo 'test1@fake test1/' >> /etc/postfix/virtual_mailbox \
+&& echo 'fallthrough@fake test2/' >> /etc/postfix/virtual_mailbox \
+&& postmap /etc/postfix/virtual_mailbox \
+&& mkdir /var/vmail \
+&& chown nobody:mail /var/vmail \
+&& postfix reload \
+&& touch /etc/dovecot/conf.d/auth-plain.conf.ext \
+&& echo 'passdb {' >> /etc/postfix/virtual_mailbox \
+&& echo '  driver = passwd-file' >> /etc/postfix/virtual_mailbox \
+&& echo '  args = username_format=%u /etc/dovecot/passwd' >> /etc/postfix/virtual_mailbox \
+&& echo '}' >> /etc/postfix/virtual_mailbox \
+&& echo 'userdb {' >> /etc/postfix/virtual_mailbox \
+&& echo '  driver = passwd-file' >> /etc/postfix/virtual_mailbox \
+&& echo '  args = username_format=%u /etc/dovecot/passwd' >> /etc/postfix/virtual_mailbox \
+&& echo '}' >> /etc/postfix/virtual_mailbox \
+&& echo '!include auth-plain.conf.ext' >> /etc/dovecot/conf.d/10-auth.conf \
+&& touch /etc/dovecot/passwd
+&& echo 'test1@fake:{SSHA}ghZpew7L4psekJyC0MUoVA3Usg0SxAjm:65534:8::/var/vmail/test1::' >> /etc/dovecot/passwd \
+&& echo 'test2@fake:{SSHA}c9yb4ibK+rpoMBR+OnoMBrNgyjD8KraL:65534:8::/var/vmail/test2::' >> /etc/dovecot/passwd \
+&& doveadm pw -s SSHA -p yourPassword \
+&& echo 'mail_location = maildir:/var/vmail/%n' >> /etc/dovecot/conf.d/10-mail.conf \
+&& /etc/init.d/dovecot restart
