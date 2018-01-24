e9b72e5ee7ef1f2a4f3226712a58d39ea6c364c5
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2018-01-24 18:42:27 +0100

    #4 ci

--- ci/docker_install_php.sh
+++ ci/docker_install_php.sh
@@ -11,7 +11,7 @@ mkdir /usr/kerberos \
 
 apt-get install -y libc-client-dev libpq-dev libxml2-dev libxslt1-dev \
 && docker-php-ext-configure pgsql -with-pgsql=/usr/local/pgsql \
-&& docker-php-ext-configure imap --with-kerberos=/usr/kerberos --with-imap-ssl \
+&& docker-php-ext-configure imap -with-kerberos -with-imap-ssl \
 && docker-php-ext-install pdo_pgsql gettext pgsql xsl xmlrpc zip imap \
 && pecl install xdebug \
 && docker-php-ext-enable xdebug
