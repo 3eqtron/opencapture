bde72a79ba64378959e31ba105d6d8cd183bfec2
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2018-01-25 09:31:05 +0100

    #4 ci

--- ci/docker_install_mail_server.sh
+++ ci/docker_install_mail_server.sh
@@ -6,7 +6,7 @@
 set -xe
 
 export DEBIAN_FRONTEND=noninteractive \
-&& apt-get install -yqq postfix dovecot-imapd \
+&& apt-get install -yqq postfix dovecot-imapd mutt \
 && export DEBIAN_FRONTEND=dialog \
 && echo 'virtual_mailbox_domains = fake' >> /etc/postfix/main.cf \
 && echo 'virtual_mailbox_base = /var/vmail' >> /etc/postfix/main.cf \
