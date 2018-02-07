9941a4eb67a3d19118b1c3736ab3506abc9aa901
Laurent Giovannoni <laurent.giovannoni@maarch.org>
2018-02-07 11:14:36 +0100

    test ci with light install

--- ci/docker_install_php.sh
+++ ci/docker_install_php.sh
@@ -9,10 +9,11 @@ apt-get install -y libkrb5-dev libc-client-dev libpq-dev libxml2-dev libxslt1-de
 && mkdir /usr/kerberos \
 && ln -s /usr/lib/x86_64-linux-gnu/mit-krb5/* /usr/kerberos \
 && docker-php-ext-configure imap --with-kerberos --with-imap-ssl \
-&& docker-php-ext-configure pgsql -with-pgsql=/usr/local/pgsql \
-&& docker-php-ext-install pdo_pgsql soap gettext pgsql xsl xmlrpc zip imap \
-&& pecl install xdebug \
-&& docker-php-ext-enable xdebug \
+#&& docker-php-ext-configure pgsql -with-pgsql=/usr/local/pgsql \
+#&& docker-php-ext-install pdo_pgsql soap gettext pgsql xsl xmlrpc zip imap \
+&& docker-php-ext-install soap xsl imap \
+#&& pecl install xdebug \
+#&& docker-php-ext-enable xdebug \
 && pear channel-discover pear.maarch.org \
 && pear install maarch/CLITools-0.3.1 \
 && pear install SOAP-0.13.0
